6e452e90077a43f1c5581080c9dd55dea3fb683b
ANDREA RICCIARDI <andrea.ricciardi@autoneum.com>
2021-06-01 13:41:22 +0200

    First Commit

---
+++ uniswap-org-master/README.md
@@ -0,0 +1,10 @@
+# uniswap-org
+
+Contains the source code for the [uniswap.org](https://uniswap.org) homepage.
+
+## Development
+
+To start the dev server, pull the code down and run:
+
+1. `yarn install`
+1. `yarn start`
---
+++ uniswap-org-master/src/utils/menu.js
@@ -0,0 +1,98 @@
+const menu = [
+  {
+    name: 'Products',
+    sublinks: [
+      {
+        name: 'App',
+        link: 'https://app.uniswap.org/#/',
+        description: 'Swap tokens and supply liquidity',
+      },
+      {
+        name: 'Analytics',
+        link: 'https://uniswap.info/',
+        description: 'Uniswap analytics and historical data',
+      },
+      {
+        name: 'Token Lists',
+        link: 'https://tokenlists.org/',
+        description: 'A new Ethereum token list standard.',
+      },
+      {
+        name: 'Unisocks',
+        link: 'https://unisocks.exchange/',
+        description: 'Dynamically priced socks',
+      },
+      {
+        name: 'Sybil',
+        link: 'https://sybil.org/#/',
+        description: 'Identities for governance',
+      },
+      {
+        name: 'Unipig',
+        link: 'https://unipig.exchange/',
+        description: 'Optimistic rollup demo',
+      },
+    ],
+  },
+  {
+    name: 'Developers',
+    sublinks: [
+      {
+        name: 'Documentation',
+        link: '/docs/v2',
+      },
+      { name: 'GitHub', link: 'https://github.com/Uniswap' },
+      { name: 'V2 Whitepaper', link: '/whitepaper.pdf' },
+      { name: 'V3 Whitepaper', link: '/whitepaper-v3.pdf' },
+      { name: 'Audit', link: '/audit.html' },
+      { name: 'Bug Bounty', link: '/bug-bounty' },
+    ],
+  },
+  {
+    name: 'Governance',
+    sublinks: [
+      {
+        name: 'UNI Token',
+        link: '/blog/uni',
+      },
+      { name: 'Forum', link: 'https://gov.uniswap.org/' },
+      { name: 'Voting Portal', link: 'https://app.uniswap.org/#/vote' },
+      { name: 'Documentation', link: '/docs/v2/governance/overview' },
+    ],
+  },
+  {
+    name: 'Community',
+    sublinks: [
+      { name: 'Discord', link: 'https://discord.gg/FCfyBSbCU5' },
+      { name: 'Twitter', link: 'https://twitter.com/Uniswap' },
+      { name: 'Reddit', link: 'https://www.reddit.com/r/Uniswap' },
+    ],
+  },
+  {
+    name: 'About',
+    sublinks: [
+      {
+        name: 'Blog',
+        link: '/blog',
+      },
+      {
+        name: 'Info',
+        link: '/about',
+      },
+      {
+        name: 'FAQ',
+        link: '/faq',
+      },
+      {
+        name: 'Jobs',
+        link: '/about#jobs',
+      },
+      {
+        name: 'Logo & Brand',
+        link: '/about#brand',
+      },
+    ],
+  },
+]
+
+module.exports = menu
